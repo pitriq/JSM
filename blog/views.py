422c8255e1eb18f2543679c6851ba146d257dfe0
Pitri <franpitri@gmail.com>
2019-06-08 20:09:57 -0300

    add blog related files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def main(request):
+    all_posts = Post.objects.filter(visible=True).order_by('date_published')
+    paginator = Paginator(all_posts, 5)
+
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'main.html', {
+                  'title': 'Blog de JSM', 'posts': posts})
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('blog/', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + \
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
