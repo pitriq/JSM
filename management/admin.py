07572a1ec470c249994e295bc789303efcee2244
Lautaro Bringas <lm.bringas98@gmail.com>
2019-06-08 19:51:13 -0300

    admin person

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from management._admin.dwelling import *
+from management._admin.income import *
+from management._admin.person import *
+
 # Register your models here.
