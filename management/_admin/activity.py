cd9ac47236345d90a8969d09716fb44c2aee45c0
Lautaro Bringas <lm.bringas98@gmail.com>
2019-06-10 16:05:33 -0300

    number of students

--- management/_admin/activity.py
+++ management/_admin/activity.py
@@ -13,10 +13,16 @@ class ActivityAdmin(admin.ModelAdmin):
     inlines = [TurnInline]
     list_filter = ('start_date',)
     search_fields = ('title',)
-    list_display = ('title', 'start_date', 'end_date')
+    list_display = ('title', 'start_date', 'end_date', 'number_of_students')
 
     def get_queryset(self, request):
         qs = super(ActivityAdmin, self).get_queryset(request)
         if not request.user.is_superuser:
             return request.user.activities.all()
         return qs
+
+    def number_of_students(self, obj):
+        print(obj)
+        return f'{obj.student.all().count()}'
+
+    number_of_students.short_description = 'Numero de inscriptos'
