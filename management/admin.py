c62d75affd6c454ca191d76897117ffbaece2178
Lautaro Bringas <lm.bringas98@gmail.com>
2019-06-10 15:04:56 -0300

    chage to relative path

--- management/admin.py
+++ management/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from management._admin.activity import *
-from management._admin.assistance import *
-from management._admin.dwelling import *
-from management._admin.income import *
-from management._admin.person import *
+from ._admin.activity import *
+from ._admin.assistance import *
+from ._admin.dwelling import *
+from ._admin.income import *
+from ._admin.person import *
 
 # Register your models here.
