f4e104ec3ad697417cecaee96cca8e8449c381f5
Pitri <franpitri@gmail.com>
2019-06-18 14:37:05 -0300

    update activity attendance module

--- management/dashboard/modules/activity_attendance.py
+++ management/dashboard/modules/activity_attendance.py
@@ -28,7 +28,7 @@ class ActivityAttendance(DashboardModule):
                 percentage += (attendance * 100.0) / total_enrolled
             attendance_data.append({
                 'title': activity.title,
-                'attendace': percentage / self.q,
+                'attendance': percentage / self.q,
             })
             activity_labels.append(activity.title)
 
